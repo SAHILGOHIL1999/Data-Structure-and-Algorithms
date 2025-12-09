469143f79dc3cb2229eaa3ea88c77dda4987f6bc
SAHILGOHIL1999 <sahil19160@gmail.com>
2025-12-09 16:15:56 +0530

    file update

---
+++ C convert C++/two_Integers.cpp
@@ -0,0 +1,21 @@
+//  Add Two Integers.
+
+#include <iostream>
+using namespace std;
+
+int main(){
+    
+    int num1, num2, sum;
+
+    cout << "Enter two integers :" ;
+    cin >> num1 >> num2;
+
+    // Method : 1
+    // sum = num1 + num2;
+    // cout << "Total Sum = " << sum << endl;
+
+    // Method : 2
+    cout << "Total Sum = " << num1 + num2 <<endl;
+
+    return 0;
+}
